78bf0d6aa24be17cb4bc1e70473ed5fce5535fd1
Metehan Gultekin <mgmetehan@hotmail.com>
2022-07-19 15:43:26 +0300

    init commit

---
+++ src/main/java/com/mgmetehan/springBanking/SpringBankingApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.springBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankingApplication.class, args);
+	}
+
+}
