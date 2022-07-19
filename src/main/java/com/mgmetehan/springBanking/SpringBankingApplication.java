2891eb3f4ce80efbc93bec35da071b81f1cf922f
Metehan Gultekin <mgmetehan@hotmail.com>
2022-07-19 17:44:26 +0300

    Add database configuration

--- src/main/java/com/mgmetehan/springBanking/SpringBankingApplication.java
+++ src/main/java/com/mgmetehan/springBanking/SpringBankingApplication.java
@@ -2,12 +2,15 @@ package com.mgmetehan.springBanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableWebSecurity
 public class SpringBankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBankingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBankingApplication.class, args);
+    }
 
 }
